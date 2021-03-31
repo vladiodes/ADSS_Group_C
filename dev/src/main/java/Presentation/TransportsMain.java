d493fcfc31798ff060ca33ac7b8dfab2454791e3
IlayGo97 <59761605+IlayGo97@users.noreply.github.com>
2021-03-31 12:08:21 +0300

    making code look better

--- dev/src/main/java/Presentation/TransportsMain.java
+++ dev/src/main/java/Presentation/TransportsMain.java
@@ -32,160 +32,40 @@ public class TransportsMain {
                 break;
             switch (option) {
                 case 1:
-                    try {
-                        System.out.println("Please enter the name of the new driver");
-                        String name = in.next();
-                        System.out.println("Please enter the id of the new driver");
-                        int id = in.nextInt();
-                        System.out.println("Please enter the license of the new driver");
-                        int license = in.nextInt();
-                        API.addDriver(name, id, license);
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    AddDriver();
                     break;
                 case 2:
-                    try {
-                        System.out.println("Please enter the address of the site");
-                        String ad = in.next();
-                        System.out.println("Please enter the contact phone number of the site (only numbers no -)");
-                        int num = in.nextInt();
-                        System.out.println("Please enter the contact name of the site");
-                        String c = in.next();
-                        System.out.println("Please enter the section the site is in");
-                        String sec = in.next();
-                        API.addSite(ad, num, c, API.getSection(sec));
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    AddSite();
                     break;
                 case 3:
-                    try {
-                        System.out.println("Please enter the plate number of the truck");
-                        int plate = in.nextInt();
-                        System.out.println("Please enter the model of the truck");
-                        String model = in.next();
-                        System.out.println("Please enter max weight for the truck (only numbers!):");
-                        int maxweight = in.nextInt();
-                        System.out.println("Please enter the type of truck");
-                        String type = in.next();
-                        System.out.println("Please enter the weight of the truck without load:");
-                        int factoryweight = in.nextInt();
-                        API.addTruck(plate, model, maxweight, type, factoryweight);
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                   AddTruck();
                     break;
                 case 4:
-                    try {
-                        System.out.println("Please enter the date of the transport");
-                        String date = in.next();
-                        Date transDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-                        Date today = new Date();
-                        while (transDate.before(today)) {
-                            System.out.println("Please enter a valid date");
-                            date = in.next();
-                            transDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
-                        }
-                        System.out.println("Please enter the weight of the transport");
-                        int weight = in.nextInt();
-                        System.out.println("Please enter the id of the driver");
-                        int driverID = in.nextInt();
-                        System.out.println("Please enter the plate number of the truck");
-                        int plateNum = in.nextInt();
-                        System.out.println("Please enter where the transport is going from");
-                        String source = in.next();
-                        System.out.println("Please follow the instructions to add item contracts to the current transportation:");
-                        ArrayList<ItemContract> contracts = makeItemContract();
-                        while (true) {
-                            try {
-                                API.addTransport(transDate, weight, driverID, plateNum, contracts, source);
-                                System.out.println("The transport was successfuly recorded!");
-                                break;
-                            } catch (Exception e) {
-                                if (e.getMessage().equals("Truck weight exceeded.")) {
-                                    System.out.println("Truck weight exceeded, would you like to discard the transport? y/n");
-                                    if (in.next().equals("y"))
-                                        break;
-                                    System.out.println("which item contract would you like to edit?");
-                                    for (int i = 0; i < contracts.size(); i++)
-                                        System.out.println(i + ". " + contracts.get(i));
-                                    int tempoption = in.nextInt();
-                                    contracts.get(tempoption).setPassed(false);
-                                    contracts.addAll(makeItemContract());
-                                    System.out.println("Please enter the new weight of the transportation");
-                                    weight = in.nextInt();
-                                } else {
-                                    System.out.println(e.getMessage());
-                                    break;
-                                }
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    AddTransport();
                     break;
                 case 5:
-                    try {
-                        System.out.println("Please enter the name of the new section");
-                        String section = in.next();
-                        API.addSection(section);
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    AddSection();
                     break;
                 case 6:
-                    try {
-                        System.out.println(API.getAllTrucks());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetAllTrucks();
                     break;
                 case 7:
-                    try {
-                        System.out.println(API.getAllDrivers());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetAllDrivers();
                     break;
                 case 8:
-                    try {
-                        System.out.println(API.getAllSites());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetAllSites();
                     break;
                 case 9:
-                    try {
-                        System.out.println(API.getAllSections());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetAllSections();
                     break;
                 case 10:
-                    try {
-                        System.out.println(API.getAllTransports());
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetAllTransports();
                     break;
                 case 11:
-                    try {
-                        System.out.println("Please enter the id of the driver");
-                        int id = in.nextInt();
-                        System.out.println(API.getTransportsOfDriver(id));
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetTransportsByDriver();
                     break;
                 case 12:
-                    try {
-                        System.out.println("Please type the Date in the following format - dd/MM/yyyy");
-                        String sDate1 = in.next();
-                        System.out.println(API.getTransportsByDate(new SimpleDateFormat("dd/MM/yyyy").parse(sDate1)));
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    GetTransportsByDate();
                     break;
                 default:
                     System.out.println("Please enter a valid number");
@@ -218,5 +98,180 @@ public class TransportsMain {
                 break;
         }
         return contracts;
+    }
+
+    public static void AddDriver(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the name of the new driver");
+            String name = in.next();
+            System.out.println("Please enter the id of the new driver");
+            int id = in.nextInt();
+            System.out.println("Please enter the license of the new driver");
+            int license = in.nextInt();
+            API.addDriver(name, id, license);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void AddSite(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the address of the site");
+            String ad = in.next();
+            System.out.println("Please enter the contact phone number of the site (only numbers no -)");
+            int num = in.nextInt();
+            System.out.println("Please enter the contact name of the site");
+            String c = in.next();
+            System.out.println("Please enter the section the site is in");
+            String sec = in.next();
+            API.addSite(ad, num, c, API.getSection(sec));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void AddTruck(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the plate number of the truck");
+            int plate = in.nextInt();
+            System.out.println("Please enter the model of the truck");
+            String model = in.next();
+            System.out.println("Please enter max weight for the truck (only numbers!):");
+            int maxweight = in.nextInt();
+            System.out.println("Please enter the type of truck");
+            String type = in.next();
+            System.out.println("Please enter the weight of the truck without load:");
+            int factoryweight = in.nextInt();
+            API.addTruck(plate, model, maxweight, type, factoryweight);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void AddTransport(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the date of the transport");
+            String date = in.next();
+            Date transDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            Date today = new Date();
+            while (transDate.before(today)) {
+                System.out.println("Please enter a valid date");
+                date = in.next();
+                transDate = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            }
+            System.out.println("Please enter the weight of the transport");
+            int weight = in.nextInt();
+            System.out.println("Please enter the id of the driver");
+            int driverID = in.nextInt();
+            System.out.println("Please enter the plate number of the truck");
+            int plateNum = in.nextInt();
+            System.out.println("Please enter where the transport is going from");
+            String source = in.next();
+            System.out.println("Please follow the instructions to add item contracts to the current transportation:");
+            ArrayList<ItemContract> contracts = makeItemContract();
+            while (true) {
+                try {
+                    API.addTransport(transDate, weight, driverID, plateNum, contracts, source);
+                    System.out.println("The transport was successfuly recorded!");
+                    break;
+                } catch (Exception e) {
+                    if (e.getMessage().equals("Truck weight exceeded.")) {
+                        System.out.println("Truck weight exceeded, would you like to discard the transport? y/n");
+                        if (in.next().equals("y"))
+                            break;
+                        System.out.println("which item contract would you like to edit?");
+                        for (int i = 0; i < contracts.size(); i++)
+                            System.out.println(i + ". " + contracts.get(i));
+                        int tempoption = in.nextInt();
+                        contracts.get(tempoption).setPassed(false);
+                        contracts.addAll(makeItemContract());
+                        System.out.println("Please enter the new weight of the transportation");
+                        weight = in.nextInt();
+                    } else {
+                        System.out.println(e.getMessage());
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void AddSection(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the name of the new section");
+            String section = in.next();
+            API.addSection(section);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetAllTrucks(){
+        try {
+            System.out.println(API.getAllTrucks());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetAllDrivers(){
+        try {
+            System.out.println(API.getAllDrivers());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetAllSites(){
+        try {
+            System.out.println(API.getAllSites());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetAllSections(){
+        try {
+            System.out.println(API.getAllSections());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetAllTransports(){
+        try {
+            System.out.println(API.getAllTransports());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetTransportsByDriver(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please enter the id of the driver");
+            int id = in.nextInt();
+            System.out.println(API.getTransportsOfDriver(id));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void GetTransportsByDate(){
+        Scanner in = new Scanner(System.in);
+        try {
+            System.out.println("Please type the Date in the following format - dd/MM/yyyy");
+            String sDate1 = in.next();
+            System.out.println(API.getTransportsByDate(new SimpleDateFormat("dd/MM/yyyy").parse(sDate1)));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
