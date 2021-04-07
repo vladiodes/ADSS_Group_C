221a7b41365a4c6bc1c97d051d9be029d72ebca5
amitmosk <63066271+amitmosk@users.noreply.github.com>
2021-04-08 01:04:04 +0300

    version 1.1

--- dev/src/main/java/main.java
+++ dev/src/main/java/main.java
@@ -1,5 +1,9 @@
+import PresentationLayer.MainWindow;
+
 public class main {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) {
+        MainWindow mainW = new MainWindow();
+        mainW.start();
     }
 }
--- dev/src/main/java/superleeInv/src/main.java
+++
@@ -1,9 +0,0 @@
-import PresentationLayer.MainWindow;
-
-public class main {
-
-    public static void main(String[] args) {
-        MainWindow mainW = new MainWindow();
-        mainW.start();
-    }
-}
