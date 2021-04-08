6ab3b24a2e85ca372fc3f013bdee01714774bf5b
tomnisim <tomnisi@post.bgu.ac.il>
2021-04-08 20:22:44 +0300

    added tests - have to check them

--- dev/src/main/java/tests/ScheduleControllerTest.java
+++ dev/src/main/java/tests/ScheduleControllerTest.java
@@ -2,47 +2,156 @@ package tests;
 
 import Business.Controllers.ScheduleController;
 import Business.Controllers.StaffController;
+import Business.Shift;
 import Business.TypeOfEmployee;
 import Business.TypeOfShift;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-class ScheduleControllerTest {
+import static org.junit.Assert.*;
+
+public class ScheduleControllerTest {
+
 
     private StaffController staffController;
     private ScheduleController scheduleController;
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() throws Exception{
         staffController = new StaffController(TypeOfEmployee.HRManager);
         scheduleController = new ScheduleController(TypeOfEmployee.HRManager, staffController);
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+
+
+    @After
+    public void tearDown() throws Exception {
     }
 
-    @org.junit.jupiter.api.Test
-    void addShift() {
+    private Date getDate()
+    {
+        Date date= null;
+        try
+        {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse("20/04/2022");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Invalid date");
+        }
+        return date;
+    }
+
+    @Test
+    public void addShift() {
+        Date date=getDate();
         TypeOfShift type = TypeOfShift.Morning;
-        Date date = new Date(1111111111);
         Map<TypeOfEmployee, Integer> constraints= new HashMap<>();
-        constraints.put(TypeOfEmployee.HRManager, 1);
-        //this.scheduleController.addShift(date,type, constraints);
+        constraints.put(TypeOfEmployee.ShiftManager, 1);
+        this.scheduleController.addShift(date,type);
+        assertEquals(1, scheduleController.getSchedule().size());
+
+        //add existing shift
+        try
+        {
+            this.scheduleController.addShift(date,type);
+        }
+        catch (Exception e)
+        {
+            assertEquals("Shift already exists", e.getMessage());
+        }
+
 
     }
 
-    @org.junit.jupiter.api.Test
-    void removeShift() {
+    @Test
+    public void removeShift_doesntExist() {
+        try
+        {
+            this.scheduleController.removeShift(getDate(), TypeOfShift.Evening);
+        }
+        catch (Exception e)
+        {
+            assertEquals("Shift doesnt exist", e.getMessage());
+        }
+
     }
 
-    @org.junit.jupiter.api.Test
-    void addEmployeeToShift() {
+    @Test
+    public void addEmployeeToShift() {
     }
 
-    @org.junit.jupiter.api.Test
-    void removeEmployeeFromShift() {
+    @Test
+    public void removeEmployeeFromShift() {
+
+        try
+        {
+            this.scheduleController.removeEmployeeFromShift("209012384", getDate(), TypeOfShift.Evening);
+        }
+        catch (Exception e)
+        {
+            assertEquals("shift doesn't contain this employee", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addConstraint_NegativeConstraint() {
+
+        //generate shift
+        this.scheduleController.addShift(getDate(), TypeOfShift.Morning);
+        try
+        {
+            this.scheduleController.addConstraint(getDate(), TypeOfShift.Morning,TypeOfEmployee.Cashier, -1 );
+        }
+        catch (Exception e)
+        {
+            assertEquals("amount of Employees must be positive", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addConstraint_shiftDoesntExist() {
+        try
+        {
+            this.scheduleController.addConstraint(getDate(), TypeOfShift.Morning, TypeOfEmployee.Storage, 5);
+        }
+        catch (Exception e)
+        {
+            assertEquals("no such shift", e.getMessage());
+        }
+
+    }
+    @Test
+    public void addConstraint_shiftManagerConstraint() {
+
+        this.scheduleController.addShift(getDate(), TypeOfShift.Morning);
+        try
+        {
+            this.scheduleController.addConstraint(getDate(), TypeOfShift.Morning, TypeOfEmployee.ShiftManager, 0);
+        }
+        catch (Exception e)
+        {
+            assertEquals("Constraint of type ShiftManager must be 1 or greater", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void removeConstraint() {
+
+        this.scheduleController.addShift(getDate(), TypeOfShift.Morning);
+        try
+        {
+            this.scheduleController.removeConstraint(getDate(), TypeOfShift.Morning, TypeOfEmployee.ShiftManager);
+        }
+        catch (Exception e)
+        {
+            assertEquals("Number of ShiftManagers in a shift must be restricted", e.getMessage());
+        }
     }
 }
--- dev/src/main/java/tests/StaffControllerTest.java
+++ dev/src/main/java/tests/StaffControllerTest.java
@@ -1,69 +1,149 @@
 package tests;
 
+import Business.Controllers.ScheduleController;
 import Business.Controllers.StaffController;
+import Business.Pair;
 import Business.TypeOfEmployee;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import Business.TypeOfShift;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
-class StaffControllerTest {
+import static org.junit.Assert.*;
 
-    @BeforeEach
-    void setUp() {
-        StaffController staffController = new StaffController(TypeOfEmployee.HRManager);
+public class StaffControllerTest {
+
+    private StaffController staffController;
+    private ScheduleController scheduleController;
+    private List<TypeOfEmployee> mainSkills;
+    private List<TypeOfEmployee> skills;
+    @Before
+    public void setUp() throws Exception{
+        staffController = new StaffController(TypeOfEmployee.HRManager);
+        scheduleController = new ScheduleController(TypeOfEmployee.HRManager, staffController);
+        mainSkills=new LinkedList<>();
+        mainSkills.add(TypeOfEmployee.HRManager);
+        this.staffController.addEmployee("Tom","Nisim", "209012384", "777/23", 13000, "Sick days :0", new Date(),mainSkills );
     }
 
-    @AfterEach
-    void tearDown() {
+    private Date getDate()
+    {
+        Date date= null;
+        try
+        {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse("20/04/2022");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Invalid date");
+        }
+        return date;
+    }
+    private Date getWrongDate()
+    {
+        Date date= null;
+        try
+        {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse("20/04/2019");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Invalid date");
+        }
+        return date;
+    }
+    @Test
+    public void addEmployee() {
+
+        skills=new LinkedList<>();
+        skills.add(TypeOfEmployee.HRManager);
+        this.staffController.addEmployee("Ofri","Arad", "123456789", "777/23", 12000, "Sick days :6", new Date(),skills );
+        //check if new employee was added to the hash map
+        assertEquals(1, this.staffController.getEmployees().size());
+
     }
 
     @Test
-    void addEmployee() {
+    public void removeEmployee() {
+
     }
 
     @Test
-    void removeEmployee() {
+    public void editFirstName() {
+        skills=new LinkedList<>();
+        skills.add(TypeOfEmployee.HRManager);
+        // add new Employee
+        this.staffController.addEmployee("Leo","Messi", "101010101", "777/23", 12000, "Sick days :6", new Date(),skills );
+        //try edit wrong first name - sole letter name
+        try
+        {
+            this.staffController.editFirstName("101010101", "C");
+        }
+        catch (Exception e)
+        {
+            assertEquals("Invalid First Name", e.getMessage());
+        }
+
     }
 
     @Test
-    void editFirstName() {
+    public void editLastName() {
     }
 
     @Test
-    void editLastName() {
+    public void editID() {
     }
 
     @Test
-    void editID() {
+    public void editBankAccountNumber() {
     }
 
     @Test
-    void editBankAccountNumber() {
+    public void editSalary() {
     }
 
     @Test
-    void editSalary() {
+    public void editEmpConditions() {
     }
 
     @Test
-    void editEmpConditions() {
+    public void addSkill() {
     }
 
     @Test
-    void addSkill() {
+    public void removeSkill() {
     }
 
     @Test
-    void removeSkill() {
+    public void addAvailableShift() {
     }
 
     @Test
-    void addAvailableShift() {
-    }
+    public void removeAvailableShift_DoesntExist() {
+        try
+        {
+            this.staffController.removeAvailableShift("209012384", new Pair<Date, TypeOfShift>(getDate(), TypeOfShift.Morning));
+        }
+        catch (Exception e)
+        {
+            assertEquals("available shift already exist", e.getMessage());
+        }
 
+    }
     @Test
-    void removeAvailableShift() {
+    public void removeAvailableShift_WrongDate() {
+        try
+        {
+            this.staffController.removeAvailableShift("209012384", new Pair<Date, TypeOfShift>(getWrongDate(), TypeOfShift.Morning));
+        }
+        catch (Exception e)
+        {
+            assertEquals("date of available shift cant be in the past", e.getMessage());
+        }
+
     }
 }
