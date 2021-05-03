b22b26e2958218bd9e7b4cc596a6b7c3d7defbad
IlayGo97 <59761605+IlayGo97@users.noreply.github.com>
2021-05-03 21:16:46 +0300

    TrucksDAO

--- dev/src/main/java/Data/DAO/DAO.java
+++ dev/src/main/java/Data/DAO/DAO.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class DAO<T> {
-    private String tableName;
+    protected String tableName;
     public abstract int insert(T Ob);
     public abstract int update(T updatedOb);
     public List<T> getAll(){
--- dev/src/main/java/Data/DAO/TrucksDAO.java
+++ dev/src/main/java/Data/DAO/TrucksDAO.java
@@ -1,16 +1,41 @@
 package Data.DAO;
 
 import Data.DTO.TruckDTO;
-
+import Data.Repository;
+import java.sql.*;
 import java.sql.ResultSet;
 
 public class TrucksDAO extends DAO<TruckDTO> {
     public int insert(TruckDTO Ob) {
-        return 0;
+        Connection conn = Repository.getInstance().connect();
+        if(Ob == null) return 0;
+        String Values = String.format("(\"%s\",%s,%s,\"%s\",\"%s\")",Ob.plateNum,Ob.factoryWeight,Ob.maxWeight,Ob.model,Ob.type);
+        Statement s;
+        try {
+            s = conn.createStatement();
+            s.executeUpdate(InstertStatement(Values));
+            return 1;
+        }
+        catch (Exception e ){
+            return 0;
+        }
     }
 
     public int update(TruckDTO updatedOb) {
-        return 0;
+        Connection conn = Repository.getInstance().connect();
+        if(updatedOb == null) return 0;
+        String updateString = String.format("UPDATE %s" +
+                        " SET \"Plate Num\"= \"%s\", \"Factory Weight\"= %s, \"Max Weight\"= %s , \"Model\"=\"%s\", \"Type\"=\"%s\" " +
+                        "WHERE \"Plate Num\" == \"%s\";",
+                tableName,updatedOb.plateNum,updatedOb.factoryWeight,updatedOb.maxWeight, updatedOb.model,updatedOb.model,updatedOb.plateNum);
+        Statement s;
+        try {
+            s = conn.createStatement();
+           return s.executeUpdate(updateString);
+        }
+        catch (Exception e ){
+            return 0;
+        }
     }
 
     public TruckDTO makeDTO(ResultSet RS) { //int plateNum, String model, int maxWeight, String type, int factoryWeight
@@ -26,6 +51,16 @@ public class TrucksDAO extends DAO<TruckDTO> {
     }
 
     public int delete(TruckDTO ob) {
-        return 0;
+        Connection conn = Repository.getInstance().connect();
+        if(ob == null) return 0;
+        String delString = String.format("DELETE FROM Trucks WHERE \"Plate Num\" == %s;",ob.plateNum);
+        Statement s;
+        try {
+            s = conn.createStatement();
+            return s.executeUpdate(delString);
+        }
+        catch (Exception e ){
+            return 0;
+        }
     }
 }
